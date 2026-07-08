743141e076af57e20f3367cf67295a6a8414772b
zerocool979 <bilalfarhani635@gmail.com>
2026-07-09 02:05:25 +0700

    Use NEXT_PUBLIC_API_URL

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_API_URL
-  baseURL: '/api'
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api'
 })
 
 api.interceptors.request.use(
